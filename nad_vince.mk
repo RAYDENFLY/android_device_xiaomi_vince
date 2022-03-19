af8b364ea837121637e374b12c68ceeadad395e4
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2022-03-20 01:58:08 +0700

    vince: Nusantara adapt

--- nad_vince.mk
+++ nad_vince.mk
@@ -1,31 +1,18 @@
 #
 
-# Copyright (C) 2017 The LineageOS Project
-
+# Copyright (C) 2022 The Nusantara Project
 #
-
 # Licensed under the Apache License, Version 2.0 (the "License");
-
 # you may not use this file except in compliance with the License.
-
 # You may obtain a copy of the License at
-
 #
-
 #      http://www.apache.org/licenses/LICENSE-2.0
-
 #
-
 # Unless required by applicable law or agreed to in writing, software
-
 # distributed under the License is distributed on an "AS IS" BASIS,
-
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
 # See the License for the specific language governing permissions and
-
 # limitations under the License.
-
 #
 
 # Inherit from those products. Most specific first.
@@ -82,11 +69,11 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
+    PRIVATE_BUILD_DESC="raydenfly-user 12 S3B1.220218.004 8242181 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/S3B1.220218.004/8242181:user/release-keys"
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
