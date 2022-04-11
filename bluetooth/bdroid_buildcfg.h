d003eb57fed48e73acef836bd6a679585c173ecf
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2022-04-11 16:19:42 +0000

    vince: rebrand to xiaomi_vince

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,10 +1,5 @@
 /*
- *
- *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
- *  Not a Contribution, Apache license notifications and license are retained
- *  for attribution purposes only.
- *
- * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,14 +16,22 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
+
 #define BTM_DEF_LOCAL_NAME   "Redmi 5 Plus"
-// Disables read remote device feature
-#define MAX_ACL_CONNECTIONS   16
+#define MAX_ACL_CONNECTIONS    16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
+
+// Wide-band speech support
+#define BTIF_HF_WBS_PREFERRED TRUE
+
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
 
+/* Disable enhanced sco commands */
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
+
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 12
-#endif
+
+#endif /* _BDROID_BUILDCFG_H */
